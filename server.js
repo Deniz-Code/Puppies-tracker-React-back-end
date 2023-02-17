a820ec386c3e1f41d2a5391b9de6649dfc783185
ThiefGang <104874053+ThiefGang@users.noreply.github.com>
2023-02-17 05:19:32 +0200

    build puppySchema

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 // import npm packages
-import express from 'express'
-import logger from 'morgan'
-import cors from 'cors'
+import "dotenv/config.js"
+import express from "express"
+import logger from "morgan"
+import cors from "cors"
+import("./config/database.js")
 
 // import routers
-import { router as indexRouter } from './routes/index.js'
+import { router as indexRouter } from "./routes/index.js"
 
 // create the express app
 const app = express()
 
 // basic middleware
 app.use(cors())
-app.use(logger('dev'))
+app.use(logger("dev"))
 app.use(express.json())
 
 // mount imported routes
-app.use('/', indexRouter)
+app.use("/", indexRouter)
 
 // handle 404 errors
 app.use(function (req, res, next) {
-  res.status(404).json({ err: 'Not found' })
+  res.status(404).json({ err: "Not found" })
 })
 
 // handle all other errors
